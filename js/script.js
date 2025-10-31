c876d08a09233cab7150ace17062b96a8cef2679
Juan José Ferreri <jferaco483@g.educaand.es>
2025-10-31 12:09:53 +0100

    Cambios Finales

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //Este es mi código js
 "use Strinct"
+/***OPERACION SUMA***/
+
 // Suma dos números y devuelve el valor
 function suma(valor1, valor2) {
 
@@ -45,6 +47,94 @@ function realizarOperacionSuma(){
 }
 
 
+/***OPERACION RESTA***/
+function resta(valor1, valor2) {
+
+    return valor1 - valor2;
+
+}
+
+// Realiza la operacion de resta
+function realizarOperacionResta(){
+
+    // comenzamos con la resta
+    console.log('Realizar operacion de resta');
+
+    //obtener numero 1
+    var num1 = document.getElementById('txtNumero1').value;
+    //console.log('numero1 : '+ num1);
+    num1 = Number(num1);
+
+    //obtener numero 2
+    var num2 = document.getElementById('txtNumero2').value;
+    num2 = Number(num2);
+
+    var resultadoResta = 0;
+    
+    //realizar la resta
+    if(!isNaN(num1) && !isNaN(num2)){
+
+        resultadoResta = resta(num1,num2);
+
+    }else{
+
+        console.log('No puedo hacer la resta');
+        return;
+
+    }
+
+    console.log("Resultado resta: " + resultadoResta);
+
+    //mostrar el resultado en etiqueta
+    var lblResultadoR = document.getElementById('resultOp'); // elemento del DOM
+    lblResultadoR.innerHTML = resultadoResta.toFixed(2); // resultado con dos decimales
+
+}
+
+/***OPERACION mULTIPLICACION***/
+
+function multiplicacion(valor1, valor2) {
+
+    return valor1 * valor2;
+
+}
+
+// Realiza la operacion de Multiplicacion
+function realizarOperacionMult(){
+
+    // comenzamos con la Multiplicacion
+    console.log('Realizar operacion de multiplicacion');
+
+    //obtener numero 1
+    var num1 = document.getElementById('txtNumero1').value;
+    //console.log('numero1 : '+ num1);
+    num1 = Number(num1);
+
+    //obtener numero 2
+    var num2 = document.getElementById('txtNumero2').value;
+    num2 = Number(num2);
+
+    var resultadoMult = 0;
+    
+    //realizar la Multiplicacion
+    if(!isNaN(num1) && !isNaN(num2)){
+
+        resultadoMult = multiplicacion(num1,num2);
+
+    }else{
+
+        console.log('No puedo hacer la multiplicacion');
+        return;
+
+    }
+
+    console.log("Resultado multiplicacion: " + resultadoMult);
+
+    //mostrar el resultado en etiqueta
+    var lblResultadoM = document.getElementById('resultOp'); // elemento del DOM
+    lblResultadoM.innerHTML = resultadoMult.toFixed(2); // resultado con dos decimales
+
+}
 // funcion inicializadora
 function init() {
 
@@ -52,6 +142,16 @@ function init() {
     var btnSumar = document.getElementById('btnSumar');
 
     btnSumar.addEventListener('click', realizarOperacionSuma);
+
+    //obtener la referencia al boton de multiplicar (btnMultiplicar)
+    var btnRestar = document.getElementById('btnRestar');
+
+    btnRestar.addEventListener('click', realizarOperacionResta);
+
+    //obtener la referencia al boton de multiplicar (btnMultiplicar)
+    var btnMultiplicar = document.getElementById('btnMultiplicar');
+
+    btnMultiplicar.addEventListener('click', realizarOperacionMult);
 
 }
 
